4270f1c004c9c590243c616797eea2f1f8408d11
Sudipta Barik <bariksudipta1997@gmail.com>
2026-03-30 02:49:37 +0530

    Add Procfile and requirements for deployment

--- config/config/settings.py
+++ config/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-8m1+qqt4cd4alm073)x7==j0=03&t)yc7cnahr-^pjyeu0s51z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
